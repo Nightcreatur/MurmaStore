75e44f34f0da9c6a1c577f86a8568ab78f2a2f2e
Nightcreatur <sandeeptimilsena333@gmail.com>
2021-10-06 23:51:09 +0545

    intial changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,22 @@
+import 'package:app/pages/homepage.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(Myapp());
+import 'login.dart';
 
-class Myapp extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Bottome',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity),
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LogIn(),
+        "/home": (context) => HomePage(),
+      },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.amber,
-        body: Stack(
-          children: [
-            Positioned(
-              child: Container(
-                width: 100,
-                height: 80,
-                child: Stack(
-                  children: [
-                    CustomPaint(
-                      child: Container(color: Colors.green),
-                    )
-                  ],
-                ),
-              ),
-              bottom: 0,
-              left: 0,
-            )
-          ],
-        ));
   }
 }
